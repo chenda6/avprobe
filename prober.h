a8944a56dd1c3bff4ec5d82e7a710aa71eb997d7
David Chen <g35dave@gmail.com>
2022-08-21 16:13:21 -0700

    cleanup

--- prober.h
+++ prober.h
@@ -7,10 +7,6 @@ extern "C"
 {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-//#include <libavutil/imgutils.h>
-//#include <libavutil/opt.h>
-//#include <libswresample/swresample.h>
-//#include <libswscale/swscale.h>
 }
 
 #include "packet.h"
