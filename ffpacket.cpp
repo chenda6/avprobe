d178b4f50e7b2548405e193714331a122b622e10
David Chen <david.chen@pluto.tv>
2022-09-14 14:46:41 -0700

    clean up cmakelist

--- ffpacket.cpp
+++ ffpacket.cpp
@@ -2,8 +2,7 @@
 #include <string>
 #include "ffpacket.h"
 
-using namespace avprobe;
-
+using avprobe::FFPacket;
 
 //------------------------------------------------------------------------------
 //
